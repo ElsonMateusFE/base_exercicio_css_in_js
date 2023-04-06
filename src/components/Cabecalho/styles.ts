cff65bc426b650d5c7fcf5f767eee257f9341de3
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2023-04-06 11:28:21 -0300

    atualizacao

---
+++ src/components/Cabecalho/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import variaveis from '../../styles/variaveis'
+
+export const Cabecalho = styled.header`
+  background-color: ${variaveis.corSecundaria};
+  color: ${variaveis.corPrincipal};
+  text-align: center;
+  padding: 24px 0;
+`
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import * as S from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <S.FormHero>
+    <S.ContainerHero>
+      <S.Titulo>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </S.Titulo>
+    </S.ContainerHero>
+  </S.FormHero>
 )
 
 export default Hero
